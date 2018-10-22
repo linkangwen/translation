4a99003aeadc149ac3866e0f6a8b40a04045353d
linkangwen <332994563@qq.com>
2018-10-22 20:40:04 +0800

    first commit

---
+++ src/components/TranslateForm.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="TranslateForm">
+    <form class="row" action="" v-on:submit="formSubmit">
+      <div class="col-xs-9 col-sm-9 col-md-9">
+        <input class="form-control input-style" type="text" v-model="textToTranslate" placeholder="输入要翻译的内容">
+      </div>
+      <div class="col-xs-2 col-sm-2 col-md-2">
+        <select class="form-control" v-model="language">
+            <option value="en">English</option>
+            <option value="ru">Russian</option>
+            <option value="ko">korear</option>
+            <option value="ja">Janpenese</option>
+        </select>
+      </div>
+      <div class="col-xs-1 col-sm-1 col-md-1">
+        <input class="btn btn-default btn-style" type="submit" value="翻译">
+      </div>  
+    </form>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'TranslateForm',
+  data:function(){
+    return{
+      textToTranslate:"",
+      language:""
+    }
+  },
+  methods:{
+    formSubmit:function(e){
+      this.$emit("formSubmit",this.textToTranslate,this.language);
+      e.preventDefault();
+    }
+  },
+  created:function(){
+    this.language = "en";
+  },
+  components: {
+    
+  }
+}
+</script>
+
+<style scoped>
+.row div{
+  padding: 0;
+
+}
+.btn-style{
+  background: #12a7ff; 
+}
+</style>
---
+++ src/components/TranslateOutput.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="TranslateOutput">
+    <h2>{{translatedText}}</h2> 
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'TranslateOutput',
+  props:[
+  "translatedText"]
+ 
+}
+</script>
+
+<style>
+#TranslateOutput{
+	margin-top: 30px;
+}
+</style>
